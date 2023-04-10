c18a86d263874f1f5fb3b67bf5e244274dbdbc79
daskrys <caperezl@ucsc.edu>
2023-04-09 20:30:34 -0700

    got the basics of the app going to add gui next

--- main.py
+++ main.py
@@ -1,1 +1,33 @@
-print('hello world')
+import requests
+
+def get_pokemon_info(pokemon):
+    url = f'https://pokeapi.co/api/v2/pokemon/{pokemon}'
+    response = requests.get(url)
+
+    if response.status_code == 200:
+        data = response.json()
+
+        return data
+    else:
+        print(f"Error: Received status code {response.status_code}")
+        
+        return None
+    
+pokemon = 'fidough'
+pokemon_data = get_pokemon_info(pokemon)
+pokemon_location = requests.get(pokemon_data['location_area_encounters']).json()
+
+if pokemon_data:
+    print(f"Name: {pokemon_data['name'].title()}")
+    print(f"Height: {pokemon_data['height']}")
+    print(f"Weight: {pokemon_data['weight']}")
+
+    print("Types:")
+    for i in pokemon_data['types']:
+        print(f"- {i['type']['name'].title()}")
+
+if pokemon_location:
+
+    print("Location: ")
+    for i in pokemon_location:
+        print(f"- {i['location_area']['name'].title()}") 
