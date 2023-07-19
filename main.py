b0f27a0f39d5b0d8b2183f8df0d1ff44e57c4708
daskrys <caperezl@ucsc.edu>
2023-07-19 15:08:39 -0700

    gui works

--- main.py
+++ main.py
@@ -1,34 +1,65 @@
 import requests
 
-def get_pokemon_info(pokemon):
-    url = f'https://pokeapi.co/api/v2/pokemon/{pokemon}'
-    response = requests.get(url)
+from kivy.app import App
+from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.button import Button
+from kivy.uix.textinput import TextInput
+from kivy.uix.label import Label
+from kivy.uix.image import Image
+from kivy.core.window import Window
 
-    if response.status_code == 200:
-        data = response.json()
+# Set the size of the window
+Window.size = (400, 600)
 
-        return data
-    else:
-        print(f"Error: Received status code {response.status_code}")
+class MyApp(App):
+
+    def build(self):
+        layout = BoxLayout(orientation='vertical')
+        bg_image = Image(source='pokeball.png')
+
+        self.result = Label(text="Enter a Pokemon", size_hint_y=None, height=300)
+        self.input = TextInput(text='', multiline=False, size_hint_y=None, height=50)
+
+        submit_button = Button(text='Submit', on_press=self.update_label, size_hint_y=None, height=50)
         
-        return None
+        layout.add_widget(bg_image)
+        layout.add_widget(self.input)
+        layout.add_widget(submit_button)
+        layout.add_widget(self.result)
+        
+        return layout
+    
+    def get_pokemon(pokemon):
+        url = f'https://pokeapi.co/api/v2/pokemon/{pokemon}'
+        response = requests.get(url)
 
-print('Enter a Pokemon: ')
-pokemon = input()
-pokemon_data = get_pokemon_info(pokemon)
-pokemon_location = requests.get(pokemon_data['location_area_encounters']).json()
+        if response.status_code == 200:
+            data = response.json()
 
-if pokemon_data:
-    print(f"Name: {pokemon_data['name'].title()}")
-    print(f"Height: {pokemon_data['height']}")
-    print(f"Weight: {pokemon_data['weight']}")
+            return data
+        else: 
+            print(f"Error: Received status code {response.status_code}")
+            
+            return None
 
-    print("Types:")
-    for i in pokemon_data['types']:
-        print(f"- {i['type']['name'].title()}")
 
-if pokemon_location:
+    def update_label(self, instance):
+        #self.result.text = "You said : " + self.input.text
+        pokemon_data = MyApp.get_pokemon(self.input.text)
+        pokemon_location = requests.get(pokemon_data['location_area_encounters']).json()
 
-    print("Location: ")
-    for i in pokemon_location:
-        print(f"- {i['location_area']['name'].title()}") 
+        if pokemon_data:
+            self.result.text = f"Name: {pokemon_data['name'].title()}\nHeight: {pokemon_data['height']}\nWeight: {pokemon_data['weight']}\nTypes:"
+
+            for i in pokemon_data['types']:
+                self.result.text += f"\n- {i['type']['name'].title()}"
+
+        if pokemon_location:
+            self.result.text += "\nLocation: "
+            
+            for i in pokemon_location:
+                self.result.text += f"\n- {i['location_area']['name'].title()}"
+
+if __name__ == '__main__':
+    MyApp().run()
+
